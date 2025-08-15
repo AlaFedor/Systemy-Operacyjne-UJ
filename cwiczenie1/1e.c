2c57cf4cc4a2e3fd521a3a9076126e2226d08b30
Ala <fedor.alicja.sp8@gmail.com>
2025-08-15 16:26:01 +0200

    Update 1e.c

--- cwiczenie1/1e.c
+++ cwiczenie1/1e.c
@@ -5,31 +5,28 @@
 #include <stdlib.h>
 
 int main(){
-printf("Identyfikatory procesu macierzystego: UID: %d, GID: %d, PID: %d, PGID: %d, PPID: %d\n",
+	printf("Identyfikatory procesu macierzystego: UID: %d, GID: %d, PID: %d, PGID: %d, PPID: %d\n",
 		 getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
 
 
-for(int i=0; i<3; i++){
-switch (fork())
-{
-case -1:
+	for(int i=0; i<3; i++){
+		switch (fork())
+		{
+			case -1:
+				perror("fork error");
+				exit(1);
 
-perror("fork error");
-exit(1);
+			case 0:
+				setpgid(getpid(), 0);
+				printf("Identyfikatory procesu potomnego: UID: %d, GID: %d, PID: %d, PGID: %d, PPID: %d\n",
+					 getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
+				break;
 
-case 0:
-setpgid(getpid(), 0);
-printf("Identyfikatory procesu potomnego: UID: %d, GID: %d, PID: %d, PGID: %d, PPID: %d\n",
-		 getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
-break;
-
-default:
-
-wait(0);
-break;
-
-};
-}
+			default:
+				wait(0);
+				break;
+		};
+	}
 return 0;
 
 }
