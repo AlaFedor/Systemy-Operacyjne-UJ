97663fb9abe5ea5fac37b90fa33206d1031056d0
Ala <fedor.alicja.sp8@gmail.com>
2025-08-15 16:17:37 +0200

    Add files via upload

---
+++ cwiczenie1/1c.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include <stdlib.h>
+
+int main(){
+printf("Identyfikatory procesu macierzystego: UID: %d, GID: %d, PID: %d, PGID: %d, PPID: %d\n",
+		 getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
+
+
+for(int i=0; i<3; i++){
+switch (fork())
+{
+case -1:
+
+perror("fork error");
+exit(1);
+
+case 0:
+sleep(5);
+printf("Identyfikatory procesu potomnego: UID: %d, GID: %d, PID: %d, PGID: %d, PPID: %d\n",
+		 getuid(), getgid(), getpid(), getpgid(getpid()), getppid());
+break;
+
+default:
+break;
+
+};
+}
+
+
+
+return 0;
+
+}
